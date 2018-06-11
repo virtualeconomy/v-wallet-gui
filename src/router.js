b10b6f6e5918e8c7f6800f7b83ba4166b011dae1
masonExcelsia <mason@excelsia.io>
2018-06-11 17:56:29 +0800

    git commit -s "add vue routers"

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Signup from './views/Signup.vue'
 import Restore from './views/Restore.vue'
+import Login from './views/Login'
 
 Vue.use(Router)
 
@@ -12,6 +13,18 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    }, {
+      path: 'login',
+      name: 'login',
+      component: Login
+    }, {
+      path: 'signup',
+      name: 'signup',
+      component: Signup
+    }, {
+      path: 'restore',
+      name: 'restore',
+      component: Restore
     }
   ]
 })
