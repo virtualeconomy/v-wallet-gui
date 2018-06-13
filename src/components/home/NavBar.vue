70d59760403d3e026c95c11901d51077d65f97a6
masonExcelsia <mason@excelsia.io>
2018-06-13 16:23:12 +0800

    using bootstrap-vue-components

---
+++ src/components/home/NavBar.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <b-navbar fixed="top"
+              type="dark"
+              variant="primary"
+              toggleable>
+      <b-navbar-toggle target="nav_dropdown_collapse"></b-navbar-toggle>
+      <b-navbar-brand class="brand-logo">
+        <img src="../../assets/imgs/logo-small.png">
+      </b-navbar-brand>
+      <b-collapse is-nav
+                  visible
+                  id="nav_dropdown_collapse">
+        <b-navbar-nav>
+          <b-nav-item-dropdown text="User"
+                               right
+                               class="user-account">
+            <b-dropdown-item href="#">Account</b-dropdown-item>
+            <b-dropdown-item href="#">Settings</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+
+</template>
+
+<script>
+import bNavbar from 'bootstrap-vue/es/components/navbar/navbar'
+import bNavbarNav from 'bootstrap-vue/es/components/navbar/navbar-nav'
+import bNavbarBrand from 'bootstrap-vue/es/components/navbar/navbar-brand'
+import bNavbarToggle from 'bootstrap-vue/es/components/navbar/navbar-toggle'
+import bCollapse from 'bootstrap-vue/es/components/collapse/collapse'
+import bNavItemDropdown from 'bootstrap-vue/es/components/nav/nav-item-dropdown'
+import bDropdownItem from 'bootstrap-vue/es/components/dropdown/dropdown-item'
+
+export default {
+    name: 'NavBar',
+    components: {
+        bNavbar,
+        bNavbarNav,
+        bNavbarBrand,
+        bNavbarToggle,
+        bCollapse,
+        bNavItemDropdown,
+        bDropdownItem
+    }
+}
+</script>
+
+<style scoped>
+.brand-logo {
+    float: left;
+    margin-left: 20px;
+    margin-top: 4px;
+}
+.user-account {
+    float: right;
+    margin-right: 20px;
+    vertical-align: middle;
+}
+
+</style>
