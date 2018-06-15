85ac76952901cd26eb09beffeefaa7398988bf14
masonExcelsia <mason@excelsia.io>
2018-06-15 11:47:29 +0800

    add network constant

--- src/constants.js
+++ src/constants.js
@@ -10,39 +10,26 @@ var __assign = (this && this.__assign) || Object.assign || function(t) {
     return t;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.MAINNET_BYTE = 'W'.charCodeAt(0);
-exports.TESTNET_BYTE = 'T'.charCodeAt(0);
-exports.INITIAL_NONCE = 0;
-exports.ADDRESS_VERSION = 1;
-exports.ALIAS_VERSION = 2;
-exports.ISSUE_TX = 3;
-exports.TRANSFER_TX = 4;
-exports.REISSUE_TX = 5;
-exports.BURN_TX = 6;
-exports.EXCHANGE_TX = 7;
-exports.LEASE_TX = 8;
-exports.CANCEL_LEASING_TX = 9;
-exports.CREATE_ALIAS_TX = 10;
-exports.MASS_TRANSFER_TX = 11;
-exports.MASS_TRANSFER_TX_VERSION = 1;
-exports.ISSUE_TX_NAME = 'issue';
-exports.TRANSFER_TX_NAME = 'transfer';
-exports.REISSUE_TX_NAME = 'reissue';
-exports.BURN_TX_NAME = 'burn';
-exports.EXCHANGE_TX_NAME = 'exchange';
-exports.LEASE_TX_NAME = 'lease';
-exports.CANCEL_LEASING_TX_NAME = 'cancelLeasing';
-exports.CREATE_ALIAS_TX_NAME = 'createAlias';
-exports.PRIVATE_KEY_LENGTH = 32;
-exports.PUBLIC_KEY_LENGTH = 32;
-exports.MINIMUM_FEE = 100000;
-exports.TRANSFER_ATTACHMENT_BYTE_LIMIT = 140;
-exports.DEFAULT_MIN_SEED_LENGTH = 25;
-exports.DEFAULT_ORDER_EXPIRATION_DAYS = 20;
-exports.DEFAULT_BASIC_CONFIG = {
-    minimumSeedLength: exports.DEFAULT_MIN_SEED_LENGTH,
+export const MAINNET_BYTE = 'W'.charCodeAt(0);
+export const TESTNET_BYTE = 'T'.charCodeAt(0);
+export const INITIAL_NONCE = 0;
+export const ADDRESS_VERSION = 1;
+export const TRANSFER_TX = 4;
+export const TRANSFER_TX_NAME = 'transfer';
+export const PRIVATE_KEY_LENGTH = 32;
+export const PUBLIC_KEY_LENGTH = 32;
+export const MINIMUM_FEE = 100000;
+export const TRANSFER_ATTACHMENT_BYTE_LIMIT = 140;
+export const DEFAULT_MIN_SEED_LENGTH = 25;
+export const DEFAULT_ORDER_EXPIRATION_DAYS = 20;
+export const DEFAULT_BASIC_CONFIG = {
     requestOffset: 0,
     requestLimit: 100,
     logLevel: 'warning',
     timeDiff: 0
 };
+export const MAINNET_NODE = '54.152.46.6:6869'
+export const TESTNET_API = '54.152.46.6:6869'
+
+export const TESTNET_NODE = '54.152.46.6:6869'
+export const TESTNET_API = '54.152.46.6:6869'
