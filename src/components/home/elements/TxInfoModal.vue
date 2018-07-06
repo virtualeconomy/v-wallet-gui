32c712e70bbb2e60eb9f6bc2f18a2b066af89a21
zlvee <barde@excelsia.io>
2018-07-06 17:34:11 +0800

    uifix txInfo

--- src/components/home/elements/TxInfoModal.vue
+++ src/components/home/elements/TxInfoModal.vue
@@ -1,10 +1,16 @@
 <template>
   <b-modal :id="'txInfoModal_' + modalId"
+           ref="infoModal"
            class="tx-modal"
            centered
            hide-header
-           ok-only>
-    <div>
+           hide-footer>
+    <div class="md-content">
+      <button
+        class="close btn-close"
+        @click="closeModal">
+        <img src="../../../assets/imgs/icons/operate/ic_close.svg">
+      </button>
       <div class="tx-title">
         <div class="tx-icon">
           <img v-if="txIcon==='sent'"
@@ -78,6 +84,11 @@ export default {
             type: String,
             default: ''
         }
+    },
+    methods: {
+        closeModal() {
+            this.$refs.infoModal.hide()
+        }
     }
 }
 </script>
@@ -85,7 +96,6 @@ export default {
 <style scoped lang="less">
 .tx-modal {
     text-align: center;
-
     .tx-title {
         padding: 24px 0;
         border-bottom: 1px solid #1111;
@@ -95,34 +105,74 @@ export default {
     }
     .tx-address {
         text-align: left;
-        border-top: 1px dotted #1111;
-        padding-top: 10px;
+        border-bottom: 1px solid #E8E9ED;
+        height: 48px;
+        padding-top: 15px;
         span {
-            float: right;
+            float:right;
+            font-size: 15px;
+            color: #4F515E;
+            letter-spacing: 0;
+            text-align: right;
+        }
+        label {
+            font-size: 15px;
+            color: #9091A3;
+            letter-spacing: 0;
         }
     }
     .tx-block {
         text-align: left;
-        border-top: 1px dotted #1111;
-        padding-top: 10px;
+        border-bottom: 1px solid #E8E9ED;
+        height: 48px;
+        padding-top: 15px;
         span {
-            float: right;
+            float:right;
+            font-size: 15px;
+            color: #4F515E;
+            letter-spacing: 0;
+            text-align: right;
+        }
+        label {
+            font-size: 15px;
+            color: #9091A3;
+            letter-spacing: 0;
         }
     }
     .tx-fee {
         text-align: left;
-        border-top: 1px dotted #1111;
-        padding-top: 10px;
+        border-bottom: 1px solid #E8E9ED;
+        height: 48px;
+        padding-top: 15px;
         span {
-            float: right;
+            float:right;
+            font-size: 15px;
+            color: #4F515E;
+            letter-spacing: 0;
+            text-align: right;
+        }
+        label {
+            font-size: 15px;
+            color: #9091A3;
+            letter-spacing: 0;
         }
     }
     .tx-attachment {
         text-align: left;
-        border-top: 1px dotted #1111;
-        padding-top: 10px;
+        border-bottom: 1px solid #E8E9ED;
+        height: 48px;
+        padding-top: 15px;
         span {
-            float: right;
+            float:right;
+            font-size: 15px;
+            color: #4F515E;
+            letter-spacing: 0;
+            text-align: right;
+        }
+        label {
+          font-size: 15px;
+          color: #9091A3;
+          letter-spacing: 0;
         }
     }
     .sent-amount {
@@ -137,5 +187,15 @@ export default {
         letter-spacing: 0;
         text-align: center;
     }
+    .md-content {
+        padding: 24px;
+        padding-top: 0;
+    }
+}
+.btn-close {
+    position: absolute;
+    right: 0;
+    margin-right: 20px;
+    margin-top: 4px;
 }
 </style>
