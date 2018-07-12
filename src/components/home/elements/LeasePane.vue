f20d1ef0aa18b226860de4d8cfe9e4670f9279a5
mason <mason@excelsia.io>
2018-07-12 15:00:38 +0800

    add imgs and leasing components

---
+++ src/components/home/elements/LeasePane.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="row justify-content-between height-full">
+    <div class="col-auto align-self-center text-left tran-head">
+      <span class="title">
+        {{ balance }} VEE
+      </span>
+      <p class="text-muted text-des mb-0">
+        Balance
+      </p>
+    </div>
+    <div class="col align-self-center text-right">
+      <b-button variant="dark"
+                class="btn-send"
+                v-b-modal.startLeasingModal>
+        <b>Start Leasing</b>
+      </b-button>
+      <b-button>
+        <img src="../../../assets/imgs/icons/wallet/ic_help.svg">
+      </b-button>
+    </div>
+    <Leasing show="false"
+             :balances="balances"
+             :cold-addresses="coldAddresses"></Leasing>
+  </div>
+</template>
+
+<script>
+import Leasing from '../modals/Leasing'
+export default {
+    name: 'LeasePane',
+    components: {
+        Leasing
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
