41651b8aac6f79004b8de3f909cb74ab3397702b
sergiojiu <785415743@qq.com>
2019-03-18 12:21:03 +0800

    modify conf

--- src/constants.js
+++ src/constants.js
@@ -39,7 +39,7 @@ export const DEFAULT_BASIC_CONFIG = {
     timeDiff: 0
 };
 
-export const NODE_IP = 'http://test.v.systems:9922'
+export const NODE_IP = '/api'
 export const ADDRESS_LENGTH = 35;
 
 export const VSYS_PRECISION = 1e8
