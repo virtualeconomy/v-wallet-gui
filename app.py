fc74b45f167624cd7e5a2c1ba70f7d879d902d33
Sheldon <41148850+SheldonYS@users.noreply.github.com>
2019-09-09 20:31:32 +0800

    Squash merge lambda commit (#306)
    add aws lambda deploy support

---
+++ app.py
@@ -0,0 +1,45 @@
+# app.py
+from urllib.parse import urlparse, urlunparse
+from flask import Flask, redirect, jsonify, render_template, url_for, request, abort, Response
+from requests import get
+
+import json
+import requests
+
+app = Flask(__name__)
+
+# Web Pages
+
+NODE_API_URL = ""
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/<path:path>")
+def index_rest(path):
+    return render_template("index.html")
+
+@app.route('/api/', defaults={'path': ''}, methods=["GET", "POST"])
+@app.route('/api/<path:path>', methods=["GET", "POST"])
+def proxy(path):
+    resp = requests.request(
+        method=request.method,
+        url=NODE_API_URL+path,
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        params=request.args,
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+
+    excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = Response(resp.content, resp.status_code, headers)
+    return response
+
+
+# include this for local dev
+if __name__ == '__main__':
+    app.run()
