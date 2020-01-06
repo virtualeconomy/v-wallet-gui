03d541fb4c3f94a0280cda9cff3d2a5765894f05
JianweiZheng <jianweizheng@pku.edu.cn>
2020-01-06 14:09:09 +0800

    add the issue and help link to get feedback from user

--- src/constants.js
+++ src/constants.js
@@ -6,4 +6,5 @@ export const INITIAL_NONCE = 0;
 export const TRANSFER_ATTACHMENT_BYTE_LIMIT = 140;
 export const TX_TEST_EXPLORER = 'https://testexplorer.v.systems/transactions/'
 export const TX_EXPLORER = 'https://explorer.v.systems/transactions/'
+export const FEEDBACK_URL = 'https://github.com/virtualeconomy/v-wallet-gui/issues/new'
 export const SHOW_UNSUPPORTED_FUNCTION = false
