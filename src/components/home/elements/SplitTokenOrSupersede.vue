630dbd5306eb4343f300c34d87750baa6c42b82a
JianweiZheng <jianweizheng@pku.edu.cn>
2020-11-24 12:05:06 +0800

    aadd functionExplain for split token

--- src/components/home/elements/SplitTokenOrSupersede.vue
+++ src/components/home/elements/SplitTokenOrSupersede.vue
@@ -595,7 +595,7 @@ export default {
         },
         dataObject() {
             let tra = this.buildTransaction(this.coldAddresses[this.address].publicKey)
-            tra['stored_tx']['functionExplain'] = 'Set issuer to ' + tra['stored_tx']['functionData'][0]['value']
+            tra['stored_tx']['functionExplain'] = this.functionName === 'Split Token' ? 'Set token unity to ' + tra['stored_tx']['functionData'][0]['value'] : 'Set issuer to ' + tra['stored_tx']['functionData'][0]['value']
             return tra
         }
     },
