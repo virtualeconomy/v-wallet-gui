2c5c13d2087680a3713cc461678150827d195ec2
JianweiZheng <jianweizheng@pku.edu.cn>
2020-12-08 13:34:20 +0800

    add functions for supporting contractManagement

--- src/components/home/modals/ContractManagement.vue
+++ src/components/home/modals/ContractManagement.vue
@@ -4,6 +4,7 @@
 
 <script>
 import Vue from 'vue'
+import { mapState } from 'vuex'
 export default {
     name: 'ContractManagement',
     prop: {
@@ -12,7 +13,8 @@ export default {
         return {
             curContractID: '',
             curContractType: '',
-            contractList: {}
+            contractList: {},
+            curContractIsValid: false
         }
     },
     created() {
@@ -21,8 +23,37 @@ export default {
         }
     },
     computed: {
+        ...mapState({
+            chain: 'chain',
+        }),
         defaultAddress() {
             return Vue.ls.get('address')
+        },
+        isExistedContract() {
+            if (JSON.parse(window.localStorage.getItem(this.defaultAddress)) != null) {
+                let contracts = JSON.parse(window.localStorage.getItem(this.defaultAddress)).contract
+                if (contracts) {
+                    for (let key in contracts) {
+                        if (this.curContractID === key) {
+                            return true
+                        }
+                    }
+                }
+            }
+            return false
+        },
+        isValidContract() {
+            if (!this.curContractID) {
+                return void 0
+            }
+            let isValid = false
+            try {
+                this.getContractInfo()
+                isValid = this.curContractIsValid
+            } catch (e) {
+                console.log(e)
+            }
+            return isValid
         }
     },
     methods: {
@@ -30,12 +61,48 @@ export default {
             this.contractList = JSON.parse(window.localStorage.getItem(this.defaultAddress)).contract
             this.curContractID = ''
             this.curContractType = ''
+            this.curContractIsValid = false
         },
         addContract() {
             let userInfo = JSON.parse(window.localStorage.getItem(this.defaultAddress))
             Vue.set(userInfo.contract, this.curContractID, this.curContractType)
             window.localStorage.setItem(this.defaultAddress, JSON.stringify(userInfo))
             this.resetData()
+        },
+        deleteContract(contractID) {
+            let userInfo = JSON.parse(window.localStorage.getItem(this.defaultAddress))
+            delete userInfo.contract[contractID]
+            window.localStorage.setItem(this.defaultAddress, JSON.stringify(userInfo))
+            this.contractList = userInfo.contract
+        },
+        closeModal() {
+            this.resetData()
+            this.$refs.contractManagementModal.hide()
+        },
+        copyText(buttonId, addrToCopy, index) {
+            this.$refs[addrToCopy][index].select()
+            window.document.execCommand('copy')
+            this.$root.$emit('bv::show::popover', buttonId)
+            setTimeout(() => {
+                this.$root.$emit('bv::hide::popover', buttonId)
+            }, 400)
+        },
+        getContractInfo() {
+            this.chain.getContractInfo(this.curContractID).then(response => {
+                if (response.error) {
+                    this.curContractIsValid = false
+                } else {
+                    this.curContractIsValid = true
+                    if (response.type === 'NonFungibleContract') {
+                        this.curContractType = 'NFT'
+                    } else if (response.type === 'LockContract') {
+                        this.curContractType = 'Lock'
+                    } else if (response.type === 'PaymentChannelContract') {
+                        this.curContractType = 'Payment'
+                    }
+                }
+            }, respError => {
+            })
         }
     }
 }
