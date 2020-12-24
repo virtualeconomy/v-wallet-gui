fa4eb3033e5af3688bab7dc79ac679e5389ce404
Haynar216 <1500012913@pku.edu.cn>
2020-12-24 10:28:57 +0800

    get Node list

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -210,7 +210,7 @@ import { mapActions, mapState } from 'vuex'
 import SendToken from './home/elements/SendToken'
 import common from '@/js-v-sdk/src/utils/common'
 import certify from '@/utils/certify'
-import { EXPLORER, TEST_EXPLORER, NETWORK_BYTE, VSYS_RATE } from '@/network'
+import { EXPLORER, TEST_EXPLORER, NETWORK_BYTE, VSYS_RATE, TEST_VSYS_RATE } from '@/network'
 export default {
     name: 'Home',
     data: function() {
@@ -244,11 +244,7 @@ export default {
         if (!this.address || !Vue.ls.get('pwd')) {
             this.$router.push('/login')
         } else {
-            if (String.fromCharCode(NETWORK_BYTE) === 'M') {
-                this.$http.get(VSYS_RATE).then(function(result) {
-                    this.nodeList = result.body.data
-                })
-            }
+            this.getNodeList()
             this.getBlockHeight()
             this.getCertifiedTokens().then(res => {
                 this.updateCertifiedTokenList(res)
@@ -317,6 +313,7 @@ export default {
     },
     computed: {
         ...mapState({
+            account: 'account',
             chain: 'chain',
             available: 'available',
             paymentRedirect: 'paymentRedirect'
@@ -545,6 +542,23 @@ export default {
                 let seed = seedLib.fromExistingPhrasesWithIndex(seedPhrase, index)
                 Vue.set(this.addresses, seed.address, index)
             }
+        },
+        getNodeList() {
+            let rateUrl = String.fromCharCode(NETWORK_BYTE) === 'T' ? TEST_VSYS_RATE : VSYS_RATE
+            this.$http.get(rateUrl).then(function(result) {
+                let nodeList = result.body.data
+                for (let index in nodeList) {
+                    let isValid = false
+                    try {
+                        isValid = this.account.checkAddress(nodeList[index].Address)
+                    } catch (e) {
+                        console.log(e)
+                    }
+                    if (isValid) {
+                        this.nodeList.push(nodeList[index])
+                    }
+                }
+            })
         },
         sortStatus() {
             if (this.sortFlag === 0) this.sortFlag = 1
--- src/network.js
+++ src/network.js
@@ -1,4 +1,6 @@
 export const NETWORK_BYTE = 'M'.charCodeAt(0);
 export const NODE_IP = '/api';
 export const VSYS_RATE = 'https://vsysrate.com/api/superNodesDetail'
+export const TEST_VSYS_RATE = 'https://test-rate.virtualeconomytech.com/api/superNodesDetail'
 export const EXPLORER = 'https://explorer.v.systems'
+export const TEST_EXPLORER = 'https://testexplorer.v.systems'
