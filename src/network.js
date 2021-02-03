e26d2939ef66a5ee13c6cc3eaa3b713bb1eda0ea
Haynar96 <54703668+Haynar96@users.noreply.github.com>
2021-02-03 17:50:14 +0800

    update url

--- src/network.js
+++ src/network.js
@@ -1,6 +1,6 @@
 export const NETWORK_BYTE = 'T'.charCodeAt(0);
 export const VSYS_RATE = 'https://vsysrate.com/api/superNodesDetail'
-export const TEST_VSYS_RATE = 'https://test-rate.virtualeconomytech.com/api/superNodesDetail'
+export const TEST_VSYS_RATE = 'https://test.vsysrate.com/api/superNodesDetail'
 export const EXPLORER = 'https://explorer.v.systems'
 export const TEST_EXPLORER = 'https://testexplorer.v.systems'
 export const TESTNET_IP = 'https://test.v.systems/api'
